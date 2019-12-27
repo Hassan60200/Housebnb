0f79238169dfa05aa7204e28f2ef4ccbfd213574
Hassan60200 <derkaouihassan19@gmail.com>
2019-12-27 16:01:56 +0100

    webpack encore js

--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    .addStyleEntry('css/app', './assets/css/app.css')
+    .addEntry('js/app', './assets/js/app.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -54,7 +53,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
